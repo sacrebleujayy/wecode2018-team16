680280ab1fea7fb51506b6d9464e80786dec0e91
Tasha R Zuniga <Tashazun@gmail.com>
2019-01-12 11:32:30 -0800

    initial structure of events

---
+++ kickstart-js-master/client/src/Events.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './Events.css'
+
+export default class Events extends React.Component {
+
+
+
+  render() {
+    const raw = new Date();
+    const month = raw.getMonth();
+
+    return (
+      <div>
+        <button>create event</button>
+        
+        <div id='modal-container'>
+          <div id='modal-content'>
+          <form>
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+            <label>
+              Date:
+              <input type='text' name='date' />
+            </label>
+            <label>
+              Address:
+              <input type='text' name='address' />
+            </label>
+            <label>
+              Description:
+              <input type='text' name='Description' />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          </div>
+        
+        </div>
+        
+        
+        
+        <h1>{month}</h1>
+        <button>arrow left</button>
+        <button> arrow right</button>
+        <ol>
+
+        </ol>
+      </div>
+
+    )
+    }
+}
